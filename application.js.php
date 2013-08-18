fe3a222f0b2ba211482c48a99702a8361406a667
Mariusz Karpicki <mkarpicki@gmail.com>
2013-08-19 01:41:37 +0200

    Trying to solve problem with scrolling on Meego, iPhone < 5 and Android

--- application.js.php
+++ application.js.php
@@ -8,14 +8,19 @@
     require_once("../../common.karpicki.com/front/component/map/js/Marker.js");
     */
 
+
     require_once("../../common.karpicki.com/front/service/geoLocation.js");
 
     require_once("../../common.karpicki.com/front/util/Request2.js");
     require_once("../../common.karpicki.com/front/util/customEvent.js");
     require_once("../../common.karpicki.com/front/util/dom.js");
+    require_once("../../common.karpicki.com/front/util/browser.js");
+
+    require_once("../../common.karpicki.com/front/lib/iscroll-lite.js");
 
     require_once("js/cashGroupDeUtil.js");
     //require_once("js/euronetPlUtil.js");
+
     require_once("js/messages.js");
     require_once("js/MapModule.js");
     require_once("js/ListModule.js");
